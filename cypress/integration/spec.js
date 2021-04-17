468c4a2dc672577a0f9b7d0734b7e0ba8d46a623
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-17 09:58:17 -0400

    rename

---
+++ cypress/integration/spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('TodoMVC', () => {
+  it('adds 2 todos', function () {
+    cy.visit('/')
+    cy.get('.new-todo').type('learn testing{enter}').type('be cool{enter}')
+    cy.get('.todo-list li').should('have.length', 2)
+  })
+})
