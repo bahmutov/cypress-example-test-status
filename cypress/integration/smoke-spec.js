caf8c8eb41f55c5e7a5f4a29320f68f161f261d5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-17 15:27:10 -0400

    fix the smoke test

--- cypress/integration/smoke-spec.js
+++ cypress/integration/smoke-spec.js
@@ -2,6 +2,7 @@
 
 describe('TodoMVC', () => {
   it('adds 2 todos', () => {
+    cy.visit('/')
     cy.get('.new-todo').type('learn testing{enter}').type('be cool{enter}')
     cy.get('.todo-list li').should('have.length', 2)
   })
