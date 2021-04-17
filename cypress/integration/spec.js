b6d7420c44f0075c654f35bb9929f70fe99b7ea4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-17 17:31:19 -0400

    add routing tests (#8)

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -309,10 +309,59 @@ describe('TodoMVC app', () => {
     })
   })
   context('routing', () => {
-    it('goes to the active items view')
-    it('respects the browser back button')
-    it('goes to the completed items view')
-    it('goes to the display all items view')
-    it('highlights the current view')
+    beforeEach(() => {
+      createDefaultTodos().as('todos')
+    })
+
+    it('goes to the active items view', () => {
+      cy.get('@todos').eq(1).find('.toggle').check()
+
+      cy.get('.filters').contains('Active').click()
+
+      cy.get('@todos').eq(0).should('contain', TODO_ITEM_ONE)
+
+      cy.get('@todos').eq(1).should('contain', TODO_ITEM_THREE)
+    })
+    it('respects the browser back button', () => {
+      cy.get('@todos').eq(1).find('.toggle').check()
+
+      cy.get('.filters').contains('Active').click()
+
+      cy.get('.filters').contains('Completed').click()
+
+      cy.get('@todos').should('have.length', 1)
+      cy.go('back')
+      cy.get('@todos').should('have.length', 2)
+      cy.go('back')
+      cy.get('@todos').should('have.length', 3)
+    })
+    it('goes to the completed items view', () => {
+      cy.get('@todos').eq(1).find('.toggle').check()
+
+      cy.get('.filters').contains('Completed').click()
+
+      cy.get('@todos').should('have.length', 1)
+    })
+    it('goes to the display all items view', () => {
+      cy.get('@todos').eq(1).find('.toggle').check()
+
+      cy.get('.filters').contains('Active').click()
+
+      cy.get('.filters').contains('Completed').click()
+
+      cy.get('.filters').contains('All').click()
+
+      cy.get('@todos').should('have.length', 3)
+    })
+    it('highlights the current view', () => {
+      // using a within here which will automatically scope
+      // nested 'cy' queries to our parent element <ul.filters>
+      cy.get('.filters').within(function () {
+        cy.contains('All').should('have.class', 'selected')
+        cy.contains('Active').click().should('have.class', 'selected')
+
+        cy.contains('Completed').click().should('have.class', 'selected')
+      })
+    })
   })
 })
